2739ef5c32c00233ee484265420bdb94c6db9ce8
Zachary Rubin <46578381+zjrubin@users.noreply.github.com>
2019-12-04 13:36:49 -0500

    Format code into functions for graph.py
    Add command line argument handling to graph.py

--- graph.py
+++ graph.py
@@ -1,22 +1,24 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from mpl_toolkits.mplot3d import Axes3D
+from typing import List, TextIO, Tuple
 
 
 def main():
-    input_file = './sample3/sample3_out.txt'
+    parser = argparse.ArgumentParser(description='Script for graphing an MST in 3D cartesian space')
+    parser.add_argument('--infile', '-i', type=str, required=True,
+                        help='The name of the input file containing the verticies and edges')
+    args = parser.parse_args()
 
-    with open(input_file, 'r') as infile:
-        num_verticies = int(infile.readline())
+    run(args.infile)
 
+
+def run(input_filename: str) -> None:
+    with open(input_filename, 'r') as infile:
         # Read in verticies
-        x, y, z = list(), list(), list()
-        for i in range(num_verticies):
-            x_, y_, z_ = infile.readline().split()
-            x.append(float(x_))
-            y.append(float(y_))
-            z.append(float(z_))
+        x, y, z = read_verticies(infile)
 
         infile.readline()  # read in extra \n
 
@@ -25,13 +27,33 @@ def main():
         print('total edge weight: ', total_edge_weight)
 
         # Read in edges
-        edges = defaultdict(list)
-        for line in infile.readlines():
-            v1, v2 = line.split()
-            v1, v2 = int(v1), int(v2)
-            edges[v1].append(v2)
+        edges = read_edges(infile)
 
-    # Create the graph
+    create_graph(x, y, z, edges)
+
+
+def read_verticies(infile: TextIO) -> Tuple[List, List, List]:
+    num_verticies = int(infile.readline())
+    x, y, z = list(), list(), list()
+    for _ in range(num_verticies):
+        x_, y_, z_ = infile.readline().split()
+        x.append(float(x_))
+        y.append(float(y_))
+        z.append(float(z_))
+    return (x, y, z)
+
+
+def read_edges(infile: TextIO) -> defaultdict(list):
+    edges = defaultdict(list)
+    for line in infile.readlines():
+        v1, v2 = line.split()
+        v1, v2 = int(v1), int(v2)
+        edges[v1].append(v2)
+    return edges
+
+
+def create_graph(x: list, y: list, z: list, edges: defaultdict) -> None:
+    # Create the figure
     plt.figure(figsize=(20,10))
     ax = plt.axes(projection='3d')
     ax.set_xlabel('X')
